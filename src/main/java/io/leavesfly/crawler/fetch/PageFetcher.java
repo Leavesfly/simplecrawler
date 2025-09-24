28b175534c35f132791d8d00cc54486342b8e0bc
山泽 <yefei.yf@alibaba-inc.com>
2025-09-24 19:18:17 +0800

    opt

--- src/main/java/io/leavesfly/crawler/fetch/PageFetcher.java
+++ src/main/java/io/leavesfly/crawler/fetch/PageFetcher.java
@@ -20,65 +20,65 @@ import io.leavesfly.crawler.fetch.store.RawPageStoreFactory;
 
 public class PageFetcher extends Fetcher {
 
-	private static final Logger LOG = LoggerFactory.getLogger(PageFetcher.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PageFetcher.class);
 
-	@Override
-	public RawPage downloadPage(String url) {
-		RawPage pawPage;
-		GetMethod method = new GetMethod(url);
-		method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
-				new DefaultHttpMethodRetryHandler(FetcherConstant.RETRY_TIMES, false));
-		if (ProxyUtil.proxyGetPageNum > FetcherConstant.SINGLE_PROXY_GET_PAGE_MAX) {
-			ProxyItem proxyItem = ProxyUtil.nextProxyItem();
-			setProxy(proxyItem);
-		}
-		try {
-			int statusCode = httpClient.executeMethod(method);
-			while (statusCode != HttpStatus.SC_OK) {
-				if (statusCode == HttpStatus.SC_FORBIDDEN) {
-					ProxyItem proxyItem = ProxyUtil.nextProxyItem();
-					setProxy(proxyItem);
-					statusCode = httpClient.executeMethod(method);
-				} else {
-					LOG.error("HttpClient error! statusCode:" + statusCode);
-					return null;
-				}
-			}
-			pawPage = generateRawPage(url, method);
+    @Override
+    public RawPage downloadPage(String url) {
+        RawPage pawPage;
+        GetMethod method = new GetMethod(url);
+        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+                new DefaultHttpMethodRetryHandler(FetcherConstant.RETRY_TIMES, false));
+        if (ProxyUtil.proxyGetPageNum > FetcherConstant.SINGLE_PROXY_GET_PAGE_MAX) {
+            ProxyItem proxyItem = ProxyUtil.nextProxyItem();
+            setProxy(proxyItem);
+        }
+        try {
+            int statusCode = httpClient.executeMethod(method);
+            while (statusCode != HttpStatus.SC_OK) {
+                if (statusCode == HttpStatus.SC_FORBIDDEN) {
+                    ProxyItem proxyItem = ProxyUtil.nextProxyItem();
+                    setProxy(proxyItem);
+                    statusCode = httpClient.executeMethod(method);
+                } else {
+                    LOG.error("HttpClient error! statusCode:" + statusCode);
+                    return null;
+                }
+            }
+            pawPage = generateRawPage(url, method);
 
-			ProxyUtil.proxyGetPageNum++;
+            ProxyUtil.proxyGetPageNum++;
 
-		} catch (HttpException e) {
-			LOG.error("HttpException occur:" + e.toString());
-			e.printStackTrace();
-			return null;
-		} catch (IOException e) {
-			LOG.error("IOException occur:" + e.toString());
-			e.printStackTrace();
-			return null;
-		} finally {
-			method.releaseConnection();
-		}
-		return pawPage;
+        } catch (HttpException e) {
+            LOG.error("HttpException occur:" + e.toString());
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            LOG.error("IOException occur:" + e.toString());
+            e.printStackTrace();
+            return null;
+        } finally {
+            method.releaseConnection();
+        }
+        return pawPage;
 
-	}
+    }
 
-	private RawPage generateRawPage(String targetURL, GetMethod method) throws IOException {
-		String contentCharSet = method.getResponseCharSet();
-		String content = method.getResponseBodyAsString(FetcherConstant.MAX_PAGE_LENGTH);
-		return new RawPage(targetURL, contentCharSet, content);
-	}
+    private RawPage generateRawPage(String targetURL, GetMethod method) throws IOException {
+        String contentCharSet = method.getResponseCharSet();
+        String content = method.getResponseBodyAsString();
+        return new RawPage(targetURL, contentCharSet, content);
+    }
 
-	@Override
-	public int storePage(RawPage rawPage) {
-		RawPageStore rawPageStore = RawPageStoreFactory
-				.generateStoreInstance(FetcherConstant.RAW_PAGE_FILE_STORE);
-		if (rawPageStore != null) {
-			int storeState = rawPageStore.store(rawPage);
-			return storeState;
-		}
+    @Override
+    public int storePage(RawPage rawPage) {
+        RawPageStore rawPageStore = RawPageStoreFactory
+                .generateStoreInstance(FetcherConstant.RAW_PAGE_FILE_STORE);
+        if (rawPageStore != null) {
+            int storeState = rawPageStore.store(rawPage);
+            return storeState;
+        }
 
-		return RawPageStore.STORE_FAILURE;
-	}
+        return RawPageStore.STORE_FAILURE;
+    }
 
 }
